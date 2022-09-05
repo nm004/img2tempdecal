4b332ab0978e3d4b02a5005e4adbb3189681cca3
Nozomi Miyamori <inspc43313@yahoo.co.jp>
2022-09-06 01:44:34 +0900

    remove dynamic scaling
    This commit uses a static scaling instead.

--- web/js/style.ts
+++ web/js/style.ts
@@ -2,7 +2,6 @@ import "./style.css";
 import "@spectrum-web-components/theme/sp-theme.js";
 import "@spectrum-web-components/theme/express/theme-dark.js";
 import "@spectrum-web-components/theme/express/theme-light.js";
-import "@spectrum-web-components/theme/express/scale-medium.js";
 import "@spectrum-web-components/theme/express/scale-large.js";
 import "@spectrum-web-components/icons-workflow/icons/sp-icon-image.js";
 import "@spectrum-web-components/icons-workflow/icons/sp-icon-help.js";
@@ -19,15 +18,11 @@ import '@spectrum-web-components/dialog/sp-dialog.js';
 import { Theme } from "@spectrum-web-components/theme";
 
 const pageTheme = document.getElementById("page-theme") as Theme;
-const preferDark = matchMedia("(prefers-color-scheme: dark)");
+pageTheme.scale = "large";
+pageTheme.theme = "express";
 
+const preferDark = matchMedia("(prefers-color-scheme: dark)");
 preferDark.addEventListener("change", function () {
   pageTheme.color = this.matches ? "dark" : "light";
 });
 preferDark.dispatchEvent(new Event("change"));
-
-const largeViewPort = matchMedia("(min-width: 832px) and (min-height: 514px)");
-largeViewPort.addEventListener("change", function () {
-  pageTheme.scale = this.matches ? "large" : "medium";
-});
-largeViewPort.dispatchEvent(new Event("change"));
